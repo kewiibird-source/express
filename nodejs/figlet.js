ca63da9fcd317943d06e8511a7211470585a0faa
kewiibird <kewiibird@gmail.com>
2026-05-15 17:31:50 +0900

    express

--- nodejs/figlet.js
+++ nodejs/figlet.js
@@ -1,10 +1,31 @@
 const figlet = require('figlet');
 
-figlet("Hello World!!", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
+// figlet("Hello World!!", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+figlet.text(
+  "Boo!",
+  {
+    font: "Upside Down Text",
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 80,
+    whitespaceBreak: true,
+  },
+  function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
   }
-  console.log(data);
-});
+);
+
+// console.dir(figlet.fontsSync(), {maxArrayLength : null});
